c7c2fb547be800fbbd4e36a6ffc7cfca6b873092
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-08 07:43:15 -0700

    Agregar archivos de proyecto.

---
+++ Consultorio RBC/Consulta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultorio_RBC
+{
+    class Consulta : Paciente
+    {
+        DateTime fecha;
+        Paciente paciente;
+        string sintomas;
+        string diagnostico;
+        string tratamiento;
+    }
+}
